928e2716a942a5ca4d5e8e6e4dee28be060e8e17
Scogent <kim159271@gmail.com>
2020-03-12 10:49:13 -0400

    Fixed up db tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_03_04_213456) do
-=======
-ActiveRecord::Schema.define(version: 2020_03_05_163230) do
->>>>>>> 279b6e656b64749b02c13571a6e506b3992d59fa
 
   create_table "cadets", force: :cascade do |t|
+    t.integer "CWID"
+    t.integer "CO_ID"
+    t.string "F_NAME"
+    t.string "M_INITIAL"
+    t.string "L_NAME"
+    t.string "R_NUM"
+    t.string "P_NUM"
+    t.string "EMAIL"
+    t.integer "RANK_ID"
   end
 
   create_table "classes", force: :cascade do |t|
@@ -27,18 +32,36 @@ ActiveRecord::Schema.define(version: 2020_03_05_163230) do
   end
 
   create_table "companies", force: :cascade do |t|
+    t.integer "CO_ID"
+    t.integer "LOCA_ID"
+    t.string "CO_NAME"
   end
 
   create_table "enrollments", force: :cascade do |t|
+    t.integer "ENROLL_ID"
+    t.integer "CWID"
+    t.integer "CLASS_ID"
   end
 
   create_table "guard_assignments", force: :cascade do |t|
+    t.integer "ASSIGN_ID"
+    t.string "DATE"
+    t.integer "LOCA_ID"
+    t.string "TIME"
+    t.integer "CWID_S"
+    t.integer "CWID_J"
+    t.integer "CWID_SR"
   end
 
   create_table "locations", force: :cascade do |t|
+    t.integer "LOCA_ID"
+    t.string "LOCA_NAME"
   end
 
   create_table "ranks", force: :cascade do |t|
+    t.integer "RANK_ID"
+    t.string "RANK_NAME"
+    t.boolean "BOL_SWORD"
   end
 
 end
