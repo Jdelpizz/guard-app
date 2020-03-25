b82e64323daf29524d3d7be7caa7213b884a34c0
Scogent <kim159271@gmail.com>
2020-03-24 22:25:36 -0400

    added in error and confirmation msgs

--- app/controllers/cadets_controller.rb
+++ app/controllers/cadets_controller.rb
@@ -9,12 +9,16 @@ class CadetsController < ApplicationController
     end
 
     def new
-
+        @cadet = Cadets.new
     end
 
     def create
         @cadet = Cadets.new(params.require(:cadet).permit(:CWID, :CO_ID, :C_STATUS, :F_NAME, :M_INITIAL, :L_NAME, :R_NUM, :P_NUM, :EMAIL, :RANK_ID))
-        @cadet.save
-        redirect_to cadets_path(@cadet)
+        if @cadet.save
+            flash[:notice] = "Cadet was created successfully."
+            redirect_to cadets_path(@cadet)
+        else
+            render 'new'
+        end
     end
 end
