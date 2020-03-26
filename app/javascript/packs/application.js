29447c2e65f70509e8f8a39cae13d5d8431734d7
Scogent <kim159271@gmail.com>
2020-03-26 02:07:26 -0400

    changed some files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap"
 require("@rails/ujs").start()
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,4 +18,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "bootstrap"
+// app/javascript/packs/application.js
