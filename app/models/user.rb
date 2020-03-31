3eb391882fdb151c0190c34ebb282ffff3bf8e7d
John Delpizzo <46827921+Jdelpizz@users.noreply.github.com>
2020-03-31 11:33:25 -0400

    Revert "User roles"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,5 @@
-class User < ApplicationRecord::Base
-  has_secure_password
-  
-  has_many :assignments
-  has_many :roles, through: :assignments
+class User < ApplicationRecord
+
+    has_secure_password
+
 end
-
-class UserTest < ActiveSupport::TestCase
-  should have_many(:assignments)
-  should have_many(:roles).through(:assignments)
-end
-
-
-def role?(role)  
-roles.any? { |r| r.name.underscore.to_sym == role }  
-end 
-
-test "user should have role" do  
-assert_not(@subject.role? :admin)
-
-@subject.roles << Role.new(name: "admin")
-
-assert(@subject.role? :admin)  
-end  
--- db/migrate/20200326070203_create_role_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoleTests < ActiveRecord::Migration[6.0]
-  def change
-    create_table :role_tests do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
