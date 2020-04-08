f17738fe5715fcd6769adcc3a3bd8f356a07e24f
jtotty <jtotty@citadel.edu>
2020-04-08 13:23:55 +0000

    Modified controller files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-
     has_secure_password
-
+    has_many :assignments  
+    has_many :roles, through: :assignments
 end
+
+def role?(role)  
+    roles.any? { |r| r.name.underscore.to_sym == role }  
+end  
