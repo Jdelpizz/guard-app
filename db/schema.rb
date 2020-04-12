c06e1df55c729b4e202d277ca48d5f9abadcf1d7
Scogent <kim159271@gmail.com>
2020-04-12 15:17:02 -0400

    Added in validation tests via rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_034737) do
+ActiveRecord::Schema.define(version: 2020_03_25_064505) do
 
   create_table "cadets", force: :cascade do |t|
     t.integer "CWID"
