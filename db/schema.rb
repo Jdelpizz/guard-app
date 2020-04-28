2911b08ff6d1a73595a97db8db351f05945f2fba
Ubuntu <ubuntu@ip-172-31-26-182.us-east-2.compute.internal>
2020-04-28 03:58:26 +0000

    commit travis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_03_28_231857) do
 
   create_table "assignemnts", force: :cascade do |t|
@@ -21,9 +20,6 @@ ActiveRecord::Schema.define(version: 2020_03_28_231857) do
     t.index ["role_id"], name: "index_assignemnts_on_role_id"
     t.index ["user_id"], name: "index_assignemnts_on_user_id"
   end
-=======
-ActiveRecord::Schema.define(version: 2020_04_06_034737) do
->>>>>>> master
 
   create_table "cadets", force: :cascade do |t|
     t.integer "CWID"
@@ -36,14 +32,11 @@ ActiveRecord::Schema.define(version: 2020_04_06_034737) do
     t.string "P_NUM"
     t.string "EMAIL"
     t.integer "RANK_ID"
-<<<<<<< HEAD
-=======
     t.string "monday"
     t.string "tuesday"
     t.string "wednesday"
     t.string "thursday"
     t.string "friday"
->>>>>>> master
   end
 
   create_table "classes", force: :cascade do |t|
@@ -86,7 +79,6 @@ ActiveRecord::Schema.define(version: 2020_04_06_034737) do
     t.boolean "BOL_SWORD"
   end
 
-<<<<<<< HEAD
   create_table "role_tests", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -99,20 +91,13 @@ ActiveRecord::Schema.define(version: 2020_04_06_034737) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-=======
->>>>>>> master
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
     t.string "role"
   end
 
   add_foreign_key "assignemnts", "users"
-=======
-  end
-
->>>>>>> master
 end
